824efcf72501a112ce1f816f8c131a941a23c655
RichardHudsonCodes <rj_hudson@hotmail.co.uk>
2022-05-20 18:09:23 +0100

    updated

--- Bet-Slipper-API/Program.cs
+++ Bet-Slipper-API/Program.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Bet_Slipper_API.Mongo;
-using Bet_Slipper_API.RepositoryService;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using MongoDB.Driver;
 
 namespace Bet_Slipper_API
 {
--- Bet-Slipper-API/Startup.cs
+++ Bet-Slipper-API/Startup.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Bet_Slipper_API.Mongo;
+﻿using Bet_Slipper_API.Mongo;
 using Bet_Slipper_API.RepositoryService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Bet_Slipper_API
