a34f2b04437dc5b776b6d67818ecaca5ed701e70
Richard hudson <rj_hudson@hotmail.co.uk>
2023-01-17 11:55:35 +0000

    removed redundant code

--- Bet-Slipper-API/Controllers/SlipsController.cs
+++ Bet-Slipper-API/Controllers/SlipsController.cs
@@ -28,30 +28,5 @@ namespace Bet_Slipper_Api.Controllers
             var multiples = generateMultiplesCommand.Execute(slip.Bets, 2);
             return multiples;
         }
-
-        private List<Bet[]> GetCombinations(IEnumerable<Bet> bets, int mulitpleNumber)
-        {
-
-            /*TODO This is the wrong implementation, what I should have done is add Ids and then create multiple
-            collections of IDs that way I could reduce duplicates that way. 
-             */
-            var result = Combinations(bets).ToList();
-
-            return result; 
-        }
-
-        public static IEnumerable<T[]> Combinations<T>(IEnumerable<T> source)
-        {
-            if (null == source)
-                throw new ArgumentNullException(nameof(source));
-
-            T[] data = source.ToArray();
-
-            return Enumerable
-              .Range(0, 1 << (data.Length))
-              .Select(index => data
-                 .Where((v, i) => (index & (1 << i)) != 0)
-                 .ToArray());
-        }
     }
 }
